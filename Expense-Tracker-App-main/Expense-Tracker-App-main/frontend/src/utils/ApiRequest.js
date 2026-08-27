73b1c5a1d517c82809019366bebd92bbe0e0e6eb
Nanditha264 <c3861282@gmail.com>
2026-08-27 23:17:06 +0530

    Update ApiRequest.js

--- Expense-Tracker-App-main/Expense-Tracker-App-main/frontend/src/utils/ApiRequest.js
+++ Expense-Tracker-App-main/Expense-Tracker-App-main/frontend/src/utils/ApiRequest.js
@@ -1,4 +1,4 @@
-const host = "http://localhost:5000";
+const host = process.env.REACT_APP_API_URL;
 // const host = "http://localhost:5000";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
